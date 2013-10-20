8efa6b2b34a999c2cd38800dee2ff7f3d3cba79e
s1022531 <s1022531@pu.edu.tw>
2013-10-20 14:56:03 +0800

    fix #13

---
+++ routes/scores.js
@@ -0,0 +1,7 @@
+@@ -4,5 +4,5 @@
+  */
+ 
+ exports.index = function(req, res){
+  
+  console.log("Query: " + scores);
+ };
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@
 /*
  * GET users listing.
  */
-
+var scores = [];
 exports.list = function(req, res){
   res.send("respond with a resource");
  };
 
 exports.save = function(req, res){
-  console.log("Your scores: " + req.url); // p.43 
+	scores.push(req.query.scores: " + req.url"); // p.43
+  console.log("Your scores: " + scores"); 
 };
